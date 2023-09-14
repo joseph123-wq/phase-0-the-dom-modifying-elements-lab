43df43bf032af7261f875519551d7df05aad18b2
JosephKagwi <josephkagwi28@gmail.com>
2023-09-14 23:50:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1')
+
+newHeader.setAttribute("id","victory");
+
+const node  = document.createTextNode("Joseph is the champion");
+newHeader.appendChild(node);
